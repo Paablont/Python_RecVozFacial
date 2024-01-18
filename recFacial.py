a222aae56beeda64f5cbc540345029dfb4e9fec0
Pablo <paablont@gmail.com>
2024-01-18 12:25:30 +0100

    Issue2 Prueba 2 de camara (se abre pero se queda pillada)

--- recFacial.py
+++ recFacial.py
@@ -3,27 +3,32 @@ import cv2
 def activarCamara(telefonoNumero):
     echarFoto(telefonoNumero)
 
-def echarFoto(telefonoNumero):
+def echarFoto(telefono):
     captura = cv2.VideoCapture(0)
-    registrado = False
-    print("Preparate, voy a hacer una foto en tres...")
-    cv2.waitKey(1000)
-    print("dos")
-    cv2.waitKey(1000)
-    print("uno")
-    while not registrado:
+    echada = False
+    while not echada:
         ok, frame = captura.read()
-        if ok:
-            guardarFoto = f"imagenes/{telefonoNumero}.jpg"
-            cv2.imwrite(guardarFoto, frame)
-            registrado = True
-            print(f"Reconocimiento facial guardado correctamente en la carpeta imagenes")
-        else:
-            print('No se ha podido recoger la imagen')
-            registrado = False
-    # do some ops
+        cv2.imshow('Cámara', frame)
+        cv2.waitKey(10000)
+
+        # Captura la imagen después de los 3 segundos
+        registrado = False
+        while not registrado:
+            ok, frame = captura.read()
+            if ok:
+                guardarFoto = f"imagenes/{telefono}.jpg"
+                cv2.imwrite(guardarFoto, frame)
+                registrado = True
+                echada = True
+                print(f"Reconocimiento facial guardado correctamente en la carpeta imagenes")
+            else:
+                print('No se ha podido recoger la imagen')
+                registrado = False
+                echada = False
+
+    # Libera los recursos de la cámara
     captura.release()
     cv2.destroyAllWindows()
 
-
+echarFoto("555")
 
