e067395f190c0cc4c07b50a5f7418cb8bf25eddb
Miguel Gomez <miguel5121996@gmail.com>
2024-01-19 19:12:48 +0100

    IssueRegistroImagen subo solucion de la camara

--- recFacial.py
+++ recFacial.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 
 def activarCamara(telefonoNumero):
@@ -6,29 +8,34 @@ def activarCamara(telefonoNumero):
 def echarFoto(telefono):
     captura = cv2.VideoCapture(0)
     echada = False
+    start_time = time.time()
+
     while not echada:
         ok, frame = captura.read()
         cv2.imshow('Cámara', frame)
-        cv2.waitKey(10000)
 
-        # Captura la imagen después de los 3 segundos
-        registrado = False
-        while not registrado:
-            ok, frame = captura.read()
-            if ok:
-                guardarFoto = f"imagenes/{telefono}.jpg"
-                cv2.imwrite(guardarFoto, frame)
-                registrado = True
-                echada = True
-                print(f"Reconocimiento facial guardado correctamente en la carpeta imagenes")
-            else:
-                print('No se ha podido recoger la imagen')
-                registrado = False
-                echada = False
+        # durante 3 segundos
+        if time.time() - start_time >= 3:
+            # Captura despues de los 3 segundos
+            registrado = False
+            while not registrado:
+                ok, frame = captura.read()
+                if ok:
+                    guardarFoto = f"imagenes/{telefono}.jpg"
+                    cv2.imwrite(guardarFoto, frame)
+                    registrado = True
+                    echada = True
+                    print(f"Reconocimiento facial guardado correctamente en la carpeta imagenes")
+                else:
+                    print('No se ha podido recoger la imagen')
+                    registrado = False
+                    echada = False
 
-    # Libera los recursos de la cámara
+        # Checkea
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        # Liberas los recursos de la camara
     captura.release()
     cv2.destroyAllWindows()
 
 echarFoto("555")
-
