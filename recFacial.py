363861a2337e97b3026fe62b4d00eba1db429f8f
Pablo <paablont@gmail.com>
2024-01-25 11:28:08 +0100

    issueComprobarIMG_CAM: Comprueba si la persona que va  a entrar al sistema esta registrada o no (es decir, existe una imagen suya en la carpeta de imagenes). Da error si la segunda imagen es un objeto (no tiene rasgos humanos)

--- recFacial.py
+++ recFacial.py
@@ -39,13 +39,47 @@ def echarFoto(telefono):
     captura.release()
     cv2.destroyAllWindows()
 
+def echarFotoComprobar():
+    captura = cv2.VideoCapture(0)
+    echada = False
+    start_time = time.time()
+
+    while not echada:
+        ok, frame = captura.read()
+        cv2.imshow('Cámara', frame)
+
+        # durante 3 segundos
+        if time.time() - start_time >= 3:
+            # Captura despues de los 3 segundos
+            registrado = False
+            while not registrado:
+                ok, frame = captura.read()
+                if ok:
+                    guardarFoto = f"foto.jpg"
+                    cv2.imwrite(guardarFoto, frame)
+                    registrado = True
+                    echada = True
+                else:
+                    print('No se ha podido recoger la imagen')
+                    registrado = False
+                    echada = False
+
+        # Checkea
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        # Liberas los recursos de la camara
+    captura.release()
+    cv2.destroyAllWindows()
+
+def borrarImagen():
+    os.remove("foto.jpg")
+
 #Este metodo pasa las imagenes de la carpeta "imagenes" a una lista
 def imagenesAlista(carpeta):
     #
     path_list = []
     files = os.listdir(carpeta)
-
-    # Filtrar solo los archivos con extensión .jpg
+    #coge los archivos .jpg,.jpeg
     image_files = [f for f in files if f.lower().endswith(('.jpg', '.jpeg'))]
 
     # Construir la lista de rutas de las imágenes
@@ -59,21 +93,37 @@ def imagenesAlista(carpeta):
     return fotos
 
 #La lista de fotos
-def asignar_perfil_color(fotos_list):
-    for i in range(len(fotos_list)):
-        fotos_list[i] = cv2.cvtColor(fotos_list[i], cv2.COLOR_BGR2RGB)
-    return fotos_list
+def asignar_perfil_color(listaImg):
+    for i in range(len(listaImg)):
+        listaImg[i] = cv2.cvtColor(listaImg[i], cv2.COLOR_BGR2RGB)
+    return listaImg
 
-def comprobarImagen(imgPath):
+
+def comprobarImagen(imgPath, listaImg):
     print("Vamos a comprobar")
-    img = cv2.imread(imgPath)
-    print(img.shape)
+    img = fr.load_image_file(imgPath)
+
+    # Obtener las características faciales de la imagen
+    img_encodings = fr.face_encodings(img)
+
+    img_encodings = img_encodings[0]
+
+    #Para
+    for i, referencia_img in enumerate(listaImg):
+            # Obtener las características faciales de la imagen de referencia
+            referencia_encodings = fr.face_encodings(referencia_img)[0]
+            resultados = fr.compare_faces([referencia_encodings], img_encodings)
+
+    return resultados
 
 
-echarFoto("777")
+#echarFoto("777")
 listaImagenes = imagenesAlista("imagenes")
-
 listaColor=asignar_perfil_color(listaImagenes)
+echarFotoComprobar()
+registrado = comprobarImagen("foto.jpg",listaColor)
+print(f"Registrado : {registrado}")
 
+borrarImagen()
 
 
